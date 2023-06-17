39bb796e4cf121465322d0e4aed414d2910e6f03
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-06-17 18:39:56 +0000

    Cleaning up scraper.py

--- scraper.py
+++ scraper.py
@@ -4,28 +4,22 @@ import re
 
 
 def scrape_website(url, num_letters):
-
     response = requests.get(url)
 
     if response.status_code == 200:
-
         soup = BeautifulSoup(response.content, 'html.parser')
+        text = soup.get_text()
+
+        pattern = r'\b[a-zA-Z]{' + str(num_letters) + r'}\b'
+        words = re.findall(pattern, text)
+        return words
+    else:
+        print("Error: Failed to retrieve the website content.")
+        return []
 
 
-text = soup.get_text()
-
-pattern = r'\b[a-zA-Z]{' + str(num_letters) + r'}\b'
-words = re.findall(pattern, text)
-return words
-else:
-
-    print("Error: Failed to retrieve the website content.")
-    return []
-
-website_url = 'https://www.dictionary.com/e/word-finder/4-letter-words/'
-
+website_url = 'https://www.example.com/'
 num_letters = int(input("Enter the number of letters to search for: "))
-
 result = scrape_website(website_url, num_letters)
 
 for word in result:
